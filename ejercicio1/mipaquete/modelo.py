fbc0ea206414c4ab6d66d45b0ed319772b577ee4
Luisa Bermeo <ferbermeo04@gmail.com>
2018-11-09 12:07:35 -0500

    Agregando segundo cambio

--- ejercicio1/mipaquete/modelo.py
+++ ejercicio1/mipaquete/modelo.py
@@ -30,6 +30,103 @@ class Empleado (object):
 		return self.comision_fija
 
 	def presentar_datos(self):
-		cadena = "Información de %s %s\n\t cedula %s"% (self.obtener_nombre(),self.obtener_apellido(),
+		cadena = "Información de %s %s\n\t CÉDULA %s"% (self.obtener_nombre(),
+														self.obtener_apellido(),
     													self.obtener_cedula())
+		return cadena
+#la clase empleado por hora hereda los atributos de la clase padre como lo es Empleado
+class EmpleadoPorHoras(Empleado):
+
+	def __init__(self):	
+		#Con la palabra super, hago referencia a la clase padre que tome en cuenta en mi nueva clas EmpleadoPorHoras
+		super(EmpleadoPorHoras,self).__init__()
+		self.numero_horas=0
+		self.valor_hora=0
+	#Declaro mis funciones agregar y obtener para los nuevos atributos	
+	def agregar_numero_horas(self,n_u):
+		self.numero_horas=n_u
+
+	def obtener_numero_horas(self):
+		return self.numero_horas
+
+	def agregar_valor_hora(self,v_h):
+		self.valor_hora=v_h
+
+	def obtener_valor_hora(self):
+		return self.valor_hora
+		#Para calcular el sueldo final tomo en cuenta mis funciones, las llamos y realizo las operaciones
+	def calcular_valor_sueldo_final(self):
+		sueldo_fijo = (self.obtener_numero_horas()*self.obtener_valor_hora()+self.obtener_comision_fija())
+		return sueldo_fijo
+
+	def presentar_datos(self):
+		#Presento cada uno de los parametros de mi clase, agregandole super para que tome en cuenta mis atributos de la clase padre
+		cadena = "%s\n Número horas: %s\n Valor hora: %s\n Sueldo final: %s"%(super(EmpleadoPorHoras,self).presentar_datos(),
+																				self.obtener_numero_horas(),
+																				self.obtener_valor_hora(),
+																				self.calcular_valor_sueldo_final())
+		#Retorno la cadena para qeu presenten los valores
+		return cadena
+class EmpleadoFijo(Empleado):
+
+	def __init__(self):	
+		#Con la palabra super, hago referencia a la clase padre que tome en cuenta en mi nueva clas EmpleadoFijo
+		super(EmpleadoFijo,self).__init__()
+		self.sueldo_fijo=0
+		self.descuento_seguro=0
+		#Declaro las funciones agregar y obtener de los nuevos atributos
+	def agregar_sueldo_fijo(self,s_f):
+		self.sueldo_fijo=s_f
+
+	def obtener_sueldo_fijo(self):
+		return self.sueldo_fijo
+
+	def agregar_descuento_seguro(self,d_s):
+		self.descuento_seguro=d_s
+
+	def obtener_descuento_seguro(self):
+		return self.descuento_seguro
+		#Calculo el sueldo final descontando el seguro social
+	def calcular_valor_sueldo_final(self):
+		sueldo_fijo = (self.obtener_sueldo_fijo()+self.obtener_comision_fija())-(self.obtener_sueldo_fijo()*(self.obtener_descuento_seguro()/100))
+		return sueldo_fijo
+		#presento valores de los atributos de la clase padre, con los nuevos atributos
+	def presentar_datos(self):
+		cadena = "%s\n Sueldo Fijo: %s\n Descuento seguro: %s\n Sueldo final: %s"%(super(EmpleadoFijo,self).presentar_datos(),
+																				self.obtener_sueldo_fijo(),
+																				self.obtener_descuento_seguro(),
+																				self.calcular_valor_sueldo_final())
+		#Retorno los valores para presentar datos
+		return cadena																																									
+
+class EmpleadoPorSemana(Empleado):
+
+	def __init__(self):
+	#Con la palabra super, hago referencia a la clase padre que tome en cuenta en mi nueva clas EmpleadoPorSemana	
+		super(EmpleadoPorSemana,self).__init__()
+		self.numero_semanas=0
+		self.valor_semanal=0
+		#Agrego las funciones obtener y agregar de los nuevos atributos de la clase hijo
+	def agregar_numero_semanas(self,n_s):
+		self.numero_semanas= n_s
+
+	def obtener_numero_semanas(self):
+		return self.numero_semanas
+
+	def agregar_valor_semanal(self,v_s):
+		self.valor_semanal= v_s
+
+	def obtener_valor_semanal(self):
+		return self.valor_semanal
+		#Calculo el sueldo final obteniendoles de las funciones obtener de los valores qeu requiero
+	def calcular_valor_sueldo_final(self):
+		sueldo_fijo=(self.obtener_numero_semanas()*self.obtener_valor_semanal()+self.obtener_comision_fija())
+		return sueldo_fijo
+		#Presento los datos tomando en cuenta los atributos de la clase padre y con los nuevos atributos de la clase hijo
+	def presentar_datos(self):
+		cadena = "%s\n Número semanas: %s\n Valor semanal: %s\n Sueldo final: %s"%(super(EmpleadoPorSemana,self).presentar_datos(),
+																				self.obtener_numero_semanas(),
+																				self.obtener_valor_semanal(),
+																				self.calcular_valor_sueldo_final())	
+		#Rertorno la cadena para presentar datos
 		return cadena
--- ejercicio1/run.py
+++ ejercicio1/run.py
@@ -1,9 +1,37 @@
+#Incorporó cada una de las clases
 from mipaquete.modelo import *
+#Creó el objeto e de la clase padre Empleado para agregarle los valores necesarios tomando en cuenta las funciones
 e = Empleado()
 e.agregar_nombre("Luis")
 e.agregar_apellido("Benitez")
-e.agregar_cedula("11001")
+e.agregar_cedula("110001")
+#Imprimo el objeto e, tomando en cuenta la función presentar datos
 print(e.presentar_datos())
+#Creó el segundo método e1 que hace referencia a la segunda clase EmpladoPorHoras, agregandole los valores y el nombre pidiendoles por teclado
+e1 = EmpleadoPorHoras()
+nombre = input("Ingresar su nombre: ")
+e1.agregar_nombre(nombre)
+e1.agregar_apellido("Andrade")
+e1.agregar_cedula("112233")
+e1.agregar_valor_hora(20.2)
+e1.agregar_numero_horas(15)
+print(e1.presentar_datos())
+#Creó el tercer objeto que hace referencia a la clase hijo EmpleadoFijo
+e2 = EmpleadoFijo()
+#Donde con la función agregar les doy sus valores respectivos 
+e2.agregar_sueldo_fijo(300.2)
+e2.descuento_seguro=10
+#pidó por teclado la comisón donde el valor que se ingresa será con decimales o float
+comision = input("Ingrese comision\n")
+comision = float(comision)
+e2.agregar_comision_fija(comision)
+e2.agregar_nombre("Ana")
+e2.agregar_apellido("Diaz")
+e2.agregar_cedula("113322")
+#Imprimo los valores
+print(e2.presentar_datos())
+
+
 
 
     
